4911d97097641b2d3ee6b6c7a9552275a14d8e8e
Kat <kyevsukov@gmail.com>
2021-10-15 13:11:04 -0400

    passing second article test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import '@testing-library/jest-dom';
+const moment = require("moment")
 
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 const testArticle =  {
     id: 123,
     headline: "Testing Article Headline",
-    createdOn: 'Friday',
-    author: "Kat",
+    createdOn: moment().subtract(Math.random()*10, "days").format(),
+    author: "Test Author",
     image: "",
     summary: "This is the test summary",
     body: "This is the test body text"   
 }
 
 test('renders component without errors', ()=> {
-    render(<Article article={{}}/>)
+    render(<Article article={{}} />)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+    render(<Article article={testArticle} />)
+
+    const headline = screen.getByTestId("headline")
+    const author = screen.getByTestId("author")
+    const summary = screen.getByTestId("summary")
+    const body = screen.getByTestId("body")
+
+    expect(headline).toBeInTheDocument()
+    expect(author).toBeInTheDocument()
+    expect(summary).toBeInTheDocument()
+    expect(body).toBeInTheDocument()
+
+    expect(headline).toHaveTextContent(/Testing Article Headline/i)
+    expect(author).toHaveTextContent(/Test Author/i)
+    expect(summary).toHaveTextContent(/This is the test summary/i)
+    expect(body).toHaveTextContent(/This is the test body text/i)
+
+});
 
 // test('renders "Associated Press" when no author is given', ()=> {
 // });
