c306bead0379068489be7fb7132040a48e3c43af
Kat <kyevsukov@gmail.com>
2021-10-15 12:51:32 -0400

    passing first article test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,8 +5,20 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
+import { render } from '@testing-library/react';
+
+const testArticle =  {
+    id: 123,
+    headline: "Testing Article Headline",
+    createdOn: 'Friday',
+    author: "Kat",
+    image: "",
+    summary: "This is the test summary",
+    body: "This is the test body text"   
+}
 
 test('renders component without errors', ()=> {
+    render(<Article article={{}}/>)
 });
 
 // test('renders headline, author from the article when passed in through props', ()=> {
