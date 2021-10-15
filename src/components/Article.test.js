c4b4f2d679d16123a4bda698df2be65236a8b9e4
Kat <kyevsukov@gmail.com>
2021-10-15 13:28:27 -0400

    passing last article test

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,56 +1,64 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-const moment = require("moment")
+import React from "react";
+import "@testing-library/jest-dom";
+const moment = require("moment");
 
-import userEvent from '@testing-library/user-event';
-import MutationObserver from 'mutationobserver-shim';
+import userEvent from "@testing-library/user-event";
+import MutationObserver from "mutationobserver-shim";
 
-import Article from './Article';
-import { render, screen } from '@testing-library/react';
+import Article from "./Article";
+import { render, screen } from "@testing-library/react";
 
-const testArticle =  {
-    id: 123,
-    headline: "Testing Article Headline",
-    createdOn: moment().subtract(Math.random()*10, "days").format(),
-    author: "Test Author",
-    image: "",
-    summary: "This is the test summary",
-    body: "This is the test body text"   
-}
+const testArticle = {
+  id: 123,
+  headline: "Testing Article Headline",
+  createdOn: moment()
+    .subtract(Math.random() * 10, "days")
+    .format(),
+  author: "Test Author",
+  image: "",
+  summary: "This is the test summary",
+  body: "This is the test body text",
+};
 
-test('renders component without errors', ()=> {
-    render(<Article article={{}} />)
+test("renders component without errors", () => {
+  render(<Article article={{}} />);
 });
 
-test('renders headline, author from the article when passed in through props', ()=> {
-    render(<Article article={testArticle} />)
+test("renders headline, author from the article when passed in through props", () => {
+  render(<Article article={testArticle} />);
 
-    const headline = screen.getByTestId("headline")
-    const author = screen.getByTestId("author")
-    const summary = screen.getByTestId("summary")
-    const body = screen.getByTestId("body")
+  const headline = screen.getByTestId("headline");
+  const author = screen.getByTestId("author");
+  const summary = screen.getByTestId("summary");
+  const body = screen.getByTestId("body");
 
-    expect(headline).toBeInTheDocument()
-    expect(author).toBeInTheDocument()
-    expect(summary).toBeInTheDocument()
-    expect(body).toBeInTheDocument()
+  expect(headline).toBeInTheDocument();
+  expect(author).toBeInTheDocument();
+  expect(summary).toBeInTheDocument();
+  expect(body).toBeInTheDocument();
 
-    expect(headline).toHaveTextContent(/Testing Article Headline/i)
-    expect(author).toHaveTextContent(/Test Author/i)
-    expect(summary).toHaveTextContent(/This is the test summary/i)
-    expect(body).toHaveTextContent(/This is the test body text/i)
-
+  expect(headline).toHaveTextContent(/Testing Article Headline/i);
+  expect(author).toHaveTextContent(/Test Author/i);
+  expect(summary).toHaveTextContent(/This is the test summary/i);
+  expect(body).toHaveTextContent(/This is the test body text/i);
 });
 
-test('renders "Associated Press" when no author is given', ()=> {
-    render(<Article article={{...testArticle, author: null}} />)
+test('renders "Associated Press" when no author is given', () => {
+  render(<Article article={{ ...testArticle, author: null }} />);
 
-    const author = screen.getByTestId('author')
-    expect(author).toHaveTextContent(/Associated Press/i)
+  const author = screen.getByTestId("author");
+  expect(author).toHaveTextContent(/Associated Press/i);
 });
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+test("executes handleDelete when the delete button is pressed", () => {
+  const mockHandleDelete = jest.fn();
+
+  render(<Article article={testArticle} handleDelete={mockHandleDelete} />);
+
+  const deleteButton = screen.getByTestId("deleteButton");
+  userEvent.click(deleteButton);
+  expect(mockHandleDelete).toBeCalledTimes(1);
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
